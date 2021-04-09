5cd180d2cbc365774c01d7ace0343a4b8eabd580
Keval <skeval904@gmail.com>
2021-04-10 03:57:17 +0530

    🗃📌

---
+++ path finder/pf.js
@@ -0,0 +1,58 @@
+var arr = new Array(5).fill(0).map(() => new Array(7).fill(0).map(() => new Array(2).fill(0)));
+var only_2_pins = new Array(2).fill(-1).map(() => new Array(2).fill(-1));
+var pinFull = 3;
+var notFound = 0;
+
+function clicked(xclass) {
+    var x, y;
+    var xcls = parseInt(xclass.substring(1));
+    x = (xcls - 1) % 5;
+    y = parseInt((xcls - 1) / 5);
+    arr[x][y][0] += 1;
+    arr[x][y][0] = arr[x][y][0] % pinFull;
+    if (arr[x][y][0] == 0) {
+        if (x == only_2_pins[0][0]) {
+            only_2_pins[0][0] = -1;
+            only_2_pins[0][1] = -1;
+            pinFull = 3;
+        } else if (x == only_2_pins[1][0]) {
+            only_2_pins[1][0] = -1;
+            only_2_pins[1][1] = -1;
+            pinFull = 3;
+        }
+        document.getElementsByClassName(xclass)[0].innerHTML = "";
+        click0(xclass);
+    } else if (arr[x][y][0] == 1) {
+        click1(xclass);
+    } else {
+        var use = document.getElementsByClassName(xclass)[0];
+        if (only_2_pins[0][0] == -1) {
+            use.innerHTML = "src";
+            only_2_pins[0][0] = x;
+            only_2_pins[0][1] = y;
+            click2(xclass);
+        } else if (only_2_pins[0][0] != -1 && only_2_pins[1][0] == -1) {
+            use.innerHTML = "dest";
+            only_2_pins[1][0] = x;
+            only_2_pins[1][1] = y;
+            pinFull = 2;
+            click2(xclass);
+        }
+    }
+}
+
+function click0(xclass) {
+    document.getElementsByClassName(xclass)[0].style.background = "white";
+}
+
+function click1(xclass) {
+    document.getElementsByClassName(xclass)[0].style.background = "#0b032d";
+}
+
+function click2(xclass) {
+    document.getElementsByClassName(xclass)[0].style.background = "#B6CEC7";
+}
+
+function fiind() {
+
+}
