5c9fa2e9eb274f7b9818f79c0e61f61b4c12abd0
Keval <skeval904@gmail.com>
2021-04-11 14:42:12 +0530

    💪obstacle tackling part1

--- path finder/pf.js
+++ path finder/pf.js
@@ -14,30 +14,21 @@ function clicked(xclass) {
     var xcls = parseInt(xclass.substring(1));
     x = (xcls - 1) % 5;
     y = parseInt((xcls - 1) / 5);
+    if (x == only_2_pins[0][0] && y == only_2_pins[0][1]) {
+        only_2_pins[0][0] = -1;
+        only_2_pins[0][1] = -1;
+        pinFull = 3;
+    } else if (x == only_2_pins[1][0] && y == only_2_pins[1][1]) {
+        only_2_pins[1][0] = -1;
+        only_2_pins[1][1] = -1;
+        pinFull = 3;
+    }
     arr[x][y][0] += 1;
     arr[x][y][0] = arr[x][y][0] % pinFull;
     if (arr[x][y][0] == 0) {
-        if (x == only_2_pins[0][0] && y == only_2_pins[0][1]) {
-            only_2_pins[0][0] = -1;
-            only_2_pins[0][1] = -1;
-            pinFull = 3;
-        } else if (x == only_2_pins[1][0] && y == only_2_pins[1][1]) {
-            only_2_pins[1][0] = -1;
-            only_2_pins[1][1] = -1;
-            pinFull = 3;
-        }
         document.getElementsByClassName(xclass)[0].innerHTML = "";
         click0(xclass);
     } else if (arr[x][y][0] == 1) {
-        if (x == only_2_pins[0][0] && y == only_2_pins[0][1]) {
-            only_2_pins[0][0] = -1;
-            only_2_pins[0][1] = -1;
-            pinFull = 3;
-        } else if (x == only_2_pins[1][0] && y == only_2_pins[1][1]) {
-            only_2_pins[1][0] = -1;
-            only_2_pins[1][1] = -1;
-            pinFull = 3;
-        }
         document.getElementsByClassName(xclass)[0].innerHTML = "";
         click1(xclass);
     } else {
@@ -47,12 +38,14 @@ function clicked(xclass) {
             only_2_pins[0][0] = x;
             only_2_pins[0][1] = y;
             click2(xclass);
+            console.log(x + "," + y);
         } else if (only_2_pins[1][0] == -1) {
             use.innerHTML = "dest";
             only_2_pins[1][0] = x;
             only_2_pins[1][1] = y;
             pinFull = 2;
             click2(xclass);
+            console.log(x + "," + y);
         }
     }
 }
@@ -77,8 +70,30 @@ function fiind() {
         console.log("1");
         return 0;
     }
-    // var currentBtn = [only_2_pins[0][0], only_2_pins[0][1]]
+
+    var currentBtn = [only_2_pins[0][0], only_2_pins[0][1]];
+
+    console.log(findDist(0, 0));
+    // var exploredlist = new Array(5).fill(-1).map(() => new Array(7).fill(-1));
     // while (notFound) {
 
     // }
+    // var dist =
+}
+
+function findDist(x, y) {
+    crntBtn = [x, y];
+    //destdist
+    var destDist = new Array(3).fill(0);
+    destDist[0] = Math.abs(crntBtn[0] - only_2_pins[1][0]);
+    destDist[1] = Math.abs(crntBtn[1] - only_2_pins[1][1]);
+    var mindist = Math.min(destDist[0], destDist[1])
+    destDist[2] = mindist * 14 + (destDist[0] - mindist) * 10 + (destDist[1] - mindist) * 10;
+    //srcdist
+    var srcDist = new Array(3).fill(0);
+    srcDist[0] = Math.abs(crntBtn[0] - only_2_pins[0][0]);
+    srcDist[1] = Math.abs(crntBtn[1] - only_2_pins[0][1]);
+    var mindist = Math.min(srcDist[0], srcDist[1])
+    srcDist[2] = mindist * 14 + (srcDist[0] - mindist) * 10 + (srcDist[1] - mindist) * 10;
+    return srcDist[2] + destDist[2];
 }
