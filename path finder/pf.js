c643ad1d529f6ce22e3d170087fa88387843b77f
Keval <skeval904@gmail.com>
2021-04-14 12:04:50 +0530

    🔢 A* Algo values used

--- path finder/pf.js
+++ path finder/pf.js
@@ -160,8 +160,8 @@ function findDist(x) {
 
     destDist[0] = Math.abs(crntBtn[0] - sd[0][0]);
     destDist[1] = Math.abs(crntBtn[1] - sd[0][1]);
-    destDist[3] = Math.min(srcDist[0], srcDist[1])
-    destDist[4] = mindist * 14 + (srcDist[0] - mindist) * 10 + (srcDist[1] - mindist) * 10;
+    destDist[3] = Math.min(destDist[0], destDist[1])
+    destDist[4] = mindist * 14 + (destDist[0] - mindist) * 10 + (destDist[1] - mindist) * 10;
     if (x[0] == sd[1][0] && x[1] == sd[1][1]) {
         return 1;
     }
