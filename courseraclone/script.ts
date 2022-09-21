ce2bf901af370a3f8cf7d4cdf7c6c9879d74c87b
Keval <skeval904@gmail.com>
2022-09-22 01:14:17 +0530

    added typescript to courseraclone

--- courseraclone/script.ts
+++ courseraclone/script.ts
@@ -1,24 +1,25 @@
-"use strict";
-const explore = document.querySelector(".explore-button");
-const overlay = document.querySelector(".overlay");
-const nav = document.querySelectorAll('.nav');
+const explore = <HTMLElement> document.querySelector(".explore-button");
+const overlay = <HTMLElement> document.querySelector(".overlay");
+const nav: NodeListOf<Element> =  document.querySelectorAll('.nav');
 // const body = document.querySelector("body");
 window.onscroll = () => {
     nav.forEach(element => {
-        window.scrollY > 0 ? element.classList.add("add-shadow") : element.classList.remove("add-shadow");
+        window.scrollY>0 ? element.classList.add("add-shadow") : element.classList.remove("add-shadow");
     });
 };
+
 const tiles = document.querySelectorAll(".tile");
 explore.onmouseover = () => {
     overlay.style.opacity = String(0.5);
     overlay.style.zIndex = String(2);
     //addition body.style.overflow = "hidden";
-};
+}
 explore.onmouseout = () => {
     overlay.style.opacity = String(0);
     overlay.style.zIndex = String(-1);
     //addition body.style.overflow = "visible";
 };
+
 // var breathe = setInterval(myTimer, 1000);
 // var x = 0;
 // function myTimer() {
@@ -28,9 +29,10 @@ explore.onmouseout = () => {
 //   document.querySelector(".items").style.marginBottom = z + "px";
 // }
 // document.querySelector(".items").style.transition = "all 1s";
-const studentLabel = document.querySelector("#student");
-const enterpriseLabel = document.querySelector("#enterprise");
+
+const studentLabel = <HTMLElement>document.querySelector("#student");
+const enterpriseLabel = <HTMLElement>document.querySelector("#enterprise");
 window.onresize = () => {
-    [studentLabel.innerHTML, enterpriseLabel.innerHTML] =
-        window.matchMedia("(max-width: 1080px)").matches ? ["Students", "Enterprise"] : ["For Students", "For Enterprise"];
+    [ studentLabel.innerHTML, enterpriseLabel.innerHTML ] = 
+        window.matchMedia("(max-width: 1080px)").matches ? [ "Students", "Enterprise" ] : [ "For Students", "For Enterprise" ]
 };
