2df765364b2d9b8c9f4676dba9e82221ac614d47
Keval <kevalthedeveloper@gmail.com>
2022-12-23 10:23:55 -0500

    moving the spbank to a separate repo so can better track changes

--- spbank/connection.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con=mysqli_connect('localhost','root','','spbank');
-
-//$con=mysqli_connect('localhost','id16891946_keval','7><(&FGmDfD=cU\x','id16891946_spbank');
-
-// if($con){
-//     die("<script>alert('Connection Failed')</script>");
-// }
-?>
